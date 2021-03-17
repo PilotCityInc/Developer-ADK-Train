1a6b49848a865147f043efed21fe78071f550dfc
Derick Lee <dericklee@pilotcity.com>
2021-03-16 19:48:38 -0700

    1.0.25

--- src/Module/components/ModuleDefault.vue
+++ src/Module/components/ModuleDefault.vue
@@ -198,7 +198,7 @@ export default defineComponent({
   &__collapse-divider {
     margin-top: 15px;
     // margin-bottom: 75px;
-    margin-bottom: none !important;
+    // margin-bottom: none !important;
     margin-right: none;
     margin-left: none;
     padding-right: none;
