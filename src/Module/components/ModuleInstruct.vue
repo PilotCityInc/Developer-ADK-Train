86c1c5e526e5268ffdea9766830e585528ee3a1f
Derick Lee <dericklee@pilotcity.com>
2021-03-21 22:50:19 -0700

    version update

--- src/Module/components/ModuleInstruct.vue
+++ src/Module/components/ModuleInstruct.vue
@@ -24,7 +24,7 @@
                   <iframe
                     width="100%"
                     height="400"
-                    src="https://www.youtube.com/embed/HnEdGFhizHg"
+                    src="https://www.youtube.com/embed/8EivHrtS0Ig"
                     frameborder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowfullscreen
